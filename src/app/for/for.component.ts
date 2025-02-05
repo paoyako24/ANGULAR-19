a6d16482e47958538d34fe1e890359af65db8ba7
paoyako24 <paolaparedes248@gmail.com>
2025-02-05 14:46:46 -0600

    Add files via upload

---
+++ src/app/for/for.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-for',
+  imports: [],
+  templateUrl: './for.component.html',
+  styleUrl: './for.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ForComponent {
+
+}
