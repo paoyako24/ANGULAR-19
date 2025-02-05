343a3be78a638aaa7cc351e731d7fd685d79f428
paoyako24 <paolaparedes248@gmail.com>
2025-02-05 17:58:45 -0600

    Add files via upload

--- src/app/for/for.component.ts
+++ src/app/for/for.component.ts
@@ -1,12 +1,13 @@
+import { NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-for',
-  imports: [],
+  imports:[NgFor],
   templateUrl: './for.component.html',
   styleUrl: './for.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ForComponent {
-
+    names: string[]= ["Maximiliano","Gabriel","Rhood","Acronimax", "PNZIDo"];
 }
