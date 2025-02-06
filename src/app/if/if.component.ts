166a36d362e28f5d18fe38c2ced2526bf45a5d66
paoyako24 <paolaparedes248@gmail.com>
2025-02-06 15:45:44 -0600

    Add files via upload

--- src/app/if/if.component.ts
+++ src/app/if/if.component.ts
@@ -1,11 +1,10 @@
 
-import { NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-if',
   standalone:true,
-  imports: [NgIf],
+  imports: [],
   templateUrl: './if.component.html',
   styleUrl: './if.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
